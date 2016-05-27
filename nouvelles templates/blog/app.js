9da48427d4287f80b439b87eab6ee242f6fef970
Opulon <frantz.salobir@orange.fr>
2016-05-27 15:30:55 +0200

    test app

--- nouvelles templates/blog/app.js
+++ nouvelles templates/blog/app.js
@@ -1,16 +1,12 @@
-app({
-  appId: 'latency',
-  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-  indexName: 'instant_search'
+
+var search = instantsearch({
+  // Replace with your own values
+  appId: 'JP7LP38V57',
+  apiKey: '0236170a81d35359b1ad085398332ff3',
+  indexName: 'postes',
+  urlSync: true
 });
 
-function app(opts) {
-  var search = instantsearch({
-    appId: opts.appId,
-    apiKey: opts.apiKey,
-    indexName: opts.indexName,
-    urlSync: true
-  });
 
   search.addWidget(
     instantsearch.widgets.searchBox({
