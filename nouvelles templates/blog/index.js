52c293999d2575dd2044d754713877569a9b138b
Opulon <frantz.salobir@orange.fr>
2016-05-30 18:36:22 +0200

    back

--- nouvelles templates/blog/index.js
+++ nouvelles templates/blog/index.js
@@ -32,11 +32,6 @@ particlesJS("particles-js",   {
 }
 }}, "interactivity": {
     "detect_on": "canvas", "events": {
-    "onresize": {
-        enable: true,
-        density_auto: true,
-        density_area: 400 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
-    }
     "onhover":  {
     "enable": true, "mode":"grab"}
 , "onclick": {
