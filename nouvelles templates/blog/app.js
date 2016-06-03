efa3c6554cc500bc3b7ba57e9b9a139b776590b0
Opulon <frantz.salobir@orange.fr>
2016-06-03 14:11:07 +0200

    app

--- nouvelles templates/blog/app.js
+++ nouvelles templates/blog/app.js
@@ -48,18 +48,6 @@ var search = instantsearch({
     })
   );
 
-  search.addWidget(
-  instantsearch.widgets.toggle({
-    container: '#Lieu',
-    attributeName: 'lieu',
-    label: 'Lieu',
-    values: {
-      on: true,
-      off: false
-    }
-  })
-);
-
 
 
 
