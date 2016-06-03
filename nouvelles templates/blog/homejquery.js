38a5c7dfbce67e6932237e620b50965209966391
Opulon <frantz.salobir@orange.fr>
2016-06-03 12:01:49 +0200

    s

--- nouvelles templates/blog/homejquery.js
+++ nouvelles templates/blog/homejquery.js
@@ -3,3 +3,11 @@ $(function(){
     $(".inactive-on-search").hide(400);
   })
 });
+
+
+$('.ais-search-box').blur(function()
+{
+    if( !$(this).val() ) {
+          $("#MerciPhilippepourlejquery").show(400);
+    }
+});
